b7809af3613bc1dfe384be10064f931ca3e84767
Mike Wetherell <mike.wetherell@tt-solutions.com>
2009-02-23 05:55:17 +0000

    Add archive class

---
+++ include/archive.h
@@ -0,0 +1,173 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        archive.h
+// Purpose:     Serialisation archive
+// Author:      Mike Wetherell
+// Modified by:
+// Created:     January 2009
+// RCS-ID:      $Id$
+// Copyright:   (c) 2009 TT-Solutions SARL
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef ARCHIVE_H
+#define ARCHIVE_H
+
+#include <wx/wx.h>
+
+#include <map>
+
+/**
+ * @file archive.h
+ * @brief Serialisation archive.
+ */
+
+namespace tt_solutions {
+
+class Archive
+{
+public:
+    class Item
+    {
+    private:
+        typedef std::map<wxString, wxString> StringMap;
+
+        Item(Archive& archive,
+             const wxString& name,
+             const wxString& id,
+             const wxString& sort);
+
+        ~Item() { SetInstance(NULL); }
+
+    public:
+        void SetClass(const wxString& name) { m_class = name; }
+        wxString GetClass() const { return m_class; }
+
+        wxString GetId() const { return m_id; }
+        wxString GetSort() const { return m_sort; }
+
+        void SetInstance(wxObject *instance, bool owns = false);
+        wxObject *GetInstance() const { return m_instance; }
+
+        Archive& GetArchive() const { return m_archive; }
+
+        bool IsStoring() const { return m_archive.IsStoring(); }
+        bool IsExtracting() const { return m_archive.IsExtracting(); }
+
+        bool Put(const wxString& name, const wxString& value = wxEmptyString);
+        bool Put(const wxString& name, const wxChar *value);
+        bool Get(const wxString& name, wxString& value) const;
+        wxString Get(const wxString& name) const;
+        bool Has(const wxString& name) const;
+        bool Remove(const wxString& name);
+
+        template <class T> bool Put(const wxString& name, const T& value) {
+            return Insert(*this, name, value);
+        }
+        template <class T> bool Get(const wxString& name, T& value) const {
+            return Extract(*this, name, value);
+        }
+
+        template <class T> T Get(const wxString& name) const {
+            T t;
+            Get(name, t);
+            return t;
+        }
+
+        template <class T>
+        void Exch(const wxString& name, T& value, const T& def = T()) {
+            if (m_archive.IsExtracting())
+                Get(name, value);
+            else if (value != def)
+                Put(name, value);
+        }
+
+        typedef StringMap::iterator iterator;
+        typedef StringMap::const_iterator const_iterator;
+
+        typedef std::pair<iterator, iterator> iterator_pair;
+        typedef std::pair<const_iterator, const_iterator> const_iterator_pair;
+
+        iterator_pair GetAttribs();
+        const_iterator_pair GetAttribs() const;
+
+    private:
+        Archive& m_archive;
+        wxString m_class;
+        wxString m_id;
+        wxString m_sort;
+        StringMap m_attribs;
+        wxObject *m_instance;
+        bool m_owns;
+
+        friend class Archive;
+    };
+
+private:
+    typedef std::map<wxString, Item*> ItemMap;
+    typedef std::multimap<wxString, Item*> SortMap;
+
+public:
+    Archive();
+    ~Archive();
+
+    void Clear();
+
+    bool Load(wxInputStream& stream);
+    bool Save(wxOutputStream& stream) const;
+
+    void SetStoring(bool storing = true) { m_storing = storing; }
+    bool IsStoring() const { return m_storing; }
+    bool IsExtracting() const { return !m_storing; }
+
+    Item *Put(const wxString& classname,
+              const wxString& id,
+              const wxString& sortkey = wxEmptyString);
+    bool Remove(const wxString& id);
+
+    Item *Get(const wxString& id);
+    const Item *Get(const wxString& id) const;
+
+    wxObject *GetInstance(const wxString& id) const;
+    static wxString MakeId(const void *p);
+
+    void SortItem(Item& item, const wxString& key);
+
+    typedef SortMap::iterator iterator;
+    typedef SortMap::const_iterator const_iterator;
+
+    typedef std::pair<iterator, iterator> iterator_pair;
+    typedef std::pair<const_iterator, const_iterator> const_iterator_pair;
+
+    iterator_pair GetItems();
+    const_iterator_pair GetItems() const;
+
+private:
+    void Sort() const;
+    void SortAdd(Item *item) const;
+    void SortRemove(Item *item) const;
+
+    ItemMap m_items;
+    mutable SortMap m_sort;
+    bool m_storing;
+};
+
+template <class T>
+bool Insert(Archive::Item& arc, const wxString& name, const T& value);
+template <class T>
+bool Extract(const Archive::Item& arc, const wxString& name, T& value);
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxPoint& value);
+bool Extract(const Archive::Item& arc, const wxString& name, wxPoint& value);
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxSize& value);
+bool Extract(const Archive::Item& arc, const wxString& name, wxSize& value);
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxColour& value);
+bool Extract(const Archive::Item& arc, const wxString& name, wxColour& value);
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxFont& value);
+bool Extract(const Archive::Item& arc, const wxString& name, wxFont& value);
+
+} // namespace tt_solutions
+
+#endif // ARCHIVE_H
---
+++ src/archive.cpp
@@ -0,0 +1,491 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        archive.cpp
+// Purpose:     Serialisation archive
+// Author:      Mike Wetherell
+// Modified by:
+// Created:     January 2009
+// RCS-ID:      $Id$
+// Copyright:   (c) 2009 TT-Solutions SARL
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#include <sstream>
+
+#include <wx/xml/xml.h>
+
+#include "archive.h"
+#include "tie.h"
+
+// ----------------------------------------------------------------------------
+// Local definitions
+// ----------------------------------------------------------------------------
+
+namespace {
+
+typedef std::basic_istringstream<wxChar> istringstream;
+typedef std::basic_ostringstream<wxChar> ostringstream;
+
+const wxChar *tagARCHIVE    = _T("archive");
+const wxChar *tagID         = _T("id");
+const wxChar *tagSORT       = _T("sort");
+
+const wxString tagFONT      = _T("wxFont");
+
+const wxChar *tagFACE       = _T("face");
+const wxChar *tagPOINTS     = _T("points");
+const wxChar *tagFAMILY     = _T("family");
+const wxChar *tagSTYLE      = _T("style");
+const wxChar *tagWEIGHT     = _T("weight");
+const wxChar *tagUNDERLINE  = _T("underline");
+const wxChar *tagENCODING   = _T("encoding");
+
+wxString FontId(const wxString& desc)
+{
+    return tagFONT + _T(" ") + desc;
+}
+
+} // namespace
+
+// ----------------------------------------------------------------------------
+// Archive
+// ----------------------------------------------------------------------------
+
+namespace tt_solutions {
+
+using std::pair;
+using std::make_pair;
+
+Archive::Archive()
+  : m_storing(true)
+{
+}
+
+Archive::~Archive()
+{
+    Clear();
+}
+
+void Archive::Clear()
+{
+    for (ItemMap::iterator it = m_items.begin(); it != m_items.end(); ++it)
+        delete it->second;
+
+    m_items.clear();
+    m_sort.clear();
+}
+
+bool Archive::Load(wxInputStream& stream)
+{
+    m_storing = false;
+    Clear();
+
+    wxXmlDocument doc;
+
+    if (!doc.Load(stream))
+        return false;
+
+    wxXmlNode *root = doc.GetRoot();
+
+    if (root->GetName() != tagARCHIVE) {
+        wxLogError(_("Error loading: unknown root node"));
+        return false;
+    }
+
+    wxXmlNode *node = root->GetChildren();
+
+    while (node) {
+        wxString name = node->GetName();
+        wxString id;
+
+        if (node->GetPropVal(tagID, &id)) {
+            wxString sortkey = node->GetPropVal(tagSORT, wxEmptyString);
+            Item *item = Put(name, id, sortkey);
+
+            if (item) {
+                wxXmlNode *attrnode = node->GetChildren();
+
+                while (attrnode) {
+                    wxString pname = attrnode->GetName();
+                    wxString value = attrnode->GetNodeContent();
+
+                    if (!item->Put(pname, value))
+                        wxLogError(_("Error loading <%s %s='%s'> ignoring duplicate <%s>"),
+                                   name.c_str(), tagID,
+                                   id.c_str(), pname.c_str());
+
+                    attrnode = attrnode->GetNext();
+                }
+            }
+            else {
+                wxLogError(_("Error loading <%s %s='%s'> id is not unique"),
+                           name.c_str(), tagID, id.c_str());
+            }
+        }
+        else {
+            wxLogError(_("Error loading <%s> missing %s"),
+                       name.c_str(), tagID);
+        }
+        node = node->GetNext();
+    }
+
+    return true;
+}
+
+bool Archive::Save(wxOutputStream& stream) const
+{
+    wxXmlNode *root = new wxXmlNode(wxXML_ELEMENT_NODE, tagARCHIVE);
+    ItemMap::const_iterator i;
+
+    for (i = m_items.begin(); i != m_items.end(); ++i) {
+        wxString id = i->first;
+        const Item *item = i->second;
+        wxString classname = item->GetClass();
+        wxString sortkey = item->GetSort();
+
+        wxXmlNode *node = new wxXmlNode(root, wxXML_ELEMENT_NODE, classname);
+        node->AddProperty(tagID, id);
+        if (!sortkey.empty())
+            node->AddProperty(tagSORT, sortkey);
+
+        Item::const_iterator j, jend;
+
+        for (tie(j, jend) = item->GetAttribs(); j != jend; ++j) {
+            wxString pname = j->first;
+            wxString value = j->second;
+
+            wxXmlNode *text = new wxXmlNode(node, wxXML_ELEMENT_NODE, pname);
+            if (!value.empty())
+                new wxXmlNode(text, wxXML_TEXT_NODE, wxEmptyString, value);
+        }
+    }
+
+    wxXmlDocument doc;
+    doc.SetRoot(root);
+    return doc.Save(stream);
+}
+
+Archive::Item *Archive::Put(const wxString& name,
+                            const wxString& id,
+                            const wxString& sort)
+{
+    Item *item = new Item(*this, name, id, sort);
+    bool added = m_items.insert(make_pair(id, item)).second;
+
+    if (added) {
+        if (m_sort.size() != 0)
+            SortAdd(item);
+        return item;
+    }
+
+    delete item;
+    return NULL;
+}
+
+bool Archive::Remove(const wxString& id)
+{
+    ItemMap::iterator it = m_items.find(id);
+
+    if (it == m_items.end())
+        return false;
+
+    SortRemove(it->second);
+    delete it->second;
+    m_items.erase(it);
+    return true;
+}
+
+Archive::Item *Archive::Get(const wxString& id)
+{
+    ItemMap::iterator it = m_items.find(id);
+    return it != m_items.end() ? it->second : NULL;
+}
+
+const Archive::Item *Archive::Get(const wxString& id) const
+{
+    ItemMap::const_iterator it = m_items.find(id);
+    return it != m_items.end() ? it->second : NULL;
+}
+
+wxObject *Archive::GetInstance(const wxString& id) const {
+    const Item *item = Get(id);
+    return item ? item->GetInstance() : NULL;
+}
+
+wxString Archive::MakeId(const void *p)
+{
+    return wxString::Format(_T("%p"), p);
+}
+
+void Archive::Sort() const
+{
+    if (m_sort.size() == 0) {
+        ItemMap::const_iterator it;
+        for (it = m_items.begin(); it != m_items.end(); ++it)
+            SortAdd(it->second);
+    }
+
+    wxASSERT(m_sort.size() == m_items.size());
+}
+
+void Archive::SortAdd(Item *item) const
+{
+    m_sort.insert(make_pair(item->GetSort(), item));
+}
+
+void Archive::SortRemove(Item *item) const
+{
+    wxString key = item->GetSort();
+    iterator it, end;
+
+    for (tie(it, end) = m_sort.equal_range(key); it != end; ++it) {
+        if (it->second == item) {
+            m_sort.erase(it);
+            break;
+        }
+    }
+}
+
+void Archive::SortItem(Item& item, const wxString& key)
+{
+    if (&item.m_archive != this)
+        return;
+
+    if (m_sort.size() != 0) {
+        SortRemove(&item);
+        item.m_sort = key;
+        SortAdd(&item);
+    }
+    else {
+        item.m_sort = key;
+    }
+}
+
+Archive::iterator_pair Archive::GetItems()
+{
+    Sort();
+    return make_pair(m_sort.begin(), m_sort.end());
+}
+
+Archive::const_iterator_pair Archive::GetItems() const
+{
+    Sort();
+    return make_pair(m_sort.begin(), m_sort.end());
+}
+
+// ----------------------------------------------------------------------------
+// Archive::Item
+// ----------------------------------------------------------------------------
+
+Archive::Item::Item(Archive& archive,
+                    const wxString& name,
+                    const wxString& id,
+                    const wxString& sort)
+  : m_archive(archive),
+    m_class(name),
+    m_id(id),
+    m_sort(sort),
+    m_instance(NULL),
+    m_owns(false)
+{
+}
+
+bool Archive::Item::Put(const wxString& name, const wxString& value)
+{
+    return m_attribs.insert(make_pair(name, value)).second;
+}
+
+bool Archive::Item::Put(const wxString& name, const wxChar *value)
+{
+    return Put(name, wxString(value));
+}
+
+bool Archive::Item::Get(const wxString& name, wxString& value) const
+{
+    const_iterator it = m_attribs.find(name);
+    if (it == m_attribs.end())
+        return false;
+    value = it->second;
+    return true;
+}
+
+wxString Archive::Item::Get(const wxString& name) const
+{
+    return Get<wxString>(name);
+}
+
+bool Archive::Item::Has(const wxString& name) const
+{
+    return m_attribs.find(name) != m_attribs.end();
+}
+
+void Archive::Item::SetInstance(wxObject *instance, bool owns)
+{
+    if (m_owns)
+        delete m_instance;
+
+    m_instance = instance;
+    m_owns = owns;
+}
+
+Archive::Item::iterator_pair Archive::Item::GetAttribs()
+{
+    return make_pair(m_attribs.begin(), m_attribs.end());
+}
+
+Archive::Item::const_iterator_pair Archive::Item::GetAttribs() const
+{
+    return make_pair(m_attribs.begin(), m_attribs.end());
+}
+
+// ----------------------------------------------------------------------------
+// Insertors/Extractors
+// ----------------------------------------------------------------------------
+
+template <class T>
+bool Insert(Archive::Item& arc, const wxString& name, const T& value)
+{
+    ostringstream ss;
+    ss << value;
+    return arc.Put(name, wxString(ss.str()));
+}
+
+template <class T>
+bool Extract(const Archive::Item& arc, const wxString& name, T& value)
+{
+    wxString str;
+    if (!arc.Get(name, str))
+        return false;
+
+    istringstream ss(str.c_str());
+    T val;
+    ss >> val;
+
+    if (ss)
+        value = val;
+
+    return ss;
+}
+
+namespace {
+
+template <class T>
+bool PutPair(Archive::Item& arc, const wxString& name, const T& value)
+{
+    return arc.Put(name, wxString() << value.x << _T(",") << value.y);
+}
+
+template <class T>
+bool GetPair(const Archive::Item& arc, const wxString& name, T& value)
+{
+    wxString str;
+    if (!arc.Get(name, str))
+        return false;
+
+    int x, y;
+    if (wxSscanf(str.c_str(), _T("%d,%d"), &x, &y) != 2)
+        return false;
+
+    value.x = x;
+    value.y = y;
+    return true;
+}
+
+} // namespace
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxPoint& value)
+{
+    return PutPair(arc, name, value);
+}
+
+bool Extract(const Archive::Item& arc, const wxString& name, wxPoint& value)
+{
+    return GetPair(arc, name, value);
+}
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxSize& value)
+{
+    return PutPair(arc, name, value);
+}
+
+bool Extract(const Archive::Item& arc, const wxString& name, wxSize& value)
+{
+    return GetPair(arc, name, value);
+}
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxColour& value)
+{
+    return arc.Put(name, value.GetAsString(wxC2S_HTML_SYNTAX));
+}
+
+bool Extract(const Archive::Item& arc, const wxString& name, wxColour& value)
+{
+    wxString str;
+    if (!arc.Get(name, str))
+        return false;
+
+    wxColour colour(str);
+    if (!colour.Ok())
+        return false;
+
+    value = colour;
+    return true;
+}
+
+bool Insert(Archive::Item& arc, const wxString& name, const wxFont& value)
+{
+    wxString desc = value.GetNativeFontInfoDesc();
+    if (!arc.Put(name, desc))
+        return false;
+
+    Archive& archive = arc.GetArchive();
+    Archive::Item *item = archive.Put(tagFONT, FontId(desc));
+
+    if (item) {
+        item->Put(tagFACE, value.GetFaceName());
+        item->Put(tagPOINTS, value.GetPointSize());
+        item->Put(tagFAMILY, value.GetFamily());
+        item->Put(tagSTYLE, value.GetStyle());
+        item->Put(tagWEIGHT, value.GetWeight());
+        if (value.GetUnderlined()) item->Put(tagUNDERLINE);
+        item->Put(tagENCODING, value.GetEncoding());
+    }
+
+    return true;
+}
+
+bool Extract(const Archive::Item& arc, const wxString& name, wxFont& value)
+{
+    wxString desc;
+    if (!arc.Get(name, desc))
+        return false;
+
+    Archive& archive = arc.GetArchive();
+    Archive::Item* item = archive.Get(FontId(desc));
+
+    if (!item)
+        return false;
+
+    wxObject *obj = item->GetInstance();
+    if (obj) {
+        value = *wxStaticCast(obj, wxFont);
+        return true;
+    }
+
+    wxFont font;
+
+    if (!font.SetNativeFontInfo(desc))
+        if (!font.Create(item->Get<int>(tagPOINTS),
+                         item->Get<int>(tagFAMILY),
+                         item->Get<int>(tagSTYLE),
+                         item->Get<int>(tagWEIGHT),
+                         item->Has(tagUNDERLINE),
+                         item->Get(tagFACE),
+                         wxFontEncoding(item->Get<int>(tagENCODING))))
+            return false;
+
+    item->SetInstance(new wxFont(font), true);
+    value = font;
+    return true;
+}
+
+} // namespace tt_solutions

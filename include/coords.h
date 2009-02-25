43fd64c5c0967cf2aad60a9c69e201d35f52cc15
Mike Wetherell <mike.wetherell@tt-solutions.com>
2009-02-25 21:32:21 +0000

    Make more general.

--- include/coords.h
+++ include/coords.h
@@ -12,6 +12,7 @@
 #ifndef COORDS_H
 #define COORDS_H
 
+#include <wx/wx.h>
 #include <limits>
 
 /**
@@ -29,36 +30,73 @@ template <int UnitsPerInch> class Coords
 public:
     enum { Inch = UnitsPerInch };
 
-    template <class T> static T FromPixels(T i, wxCoord dpi) {
-        return (i * Inch + (isint(i) && i > 0 ? dpi - 1 : 0)) / dpi ;
+    template <class C, class T> static T From(T i, int dpi) {
+        return Trans<Inch, C::Inch, T>::From(i, dpi);
     }
-    template <class T> static T ToPixels(T i, wxCoord dpi) {
-        return (i * dpi - (isint(i) && i < 0 ? Inch - 1 : 0)) / Inch;
+    template <class C, class T> static T To(T i, int dpi) {
+        return Trans<Inch, C::Inch, T>::To(i, dpi);
     }
-    template <class T> static T FromPixels(const T& pt, const wxSize& dpi) {
-        return T(FromPixels(pt.x, dpi.x), FromPixels(pt.y, dpi.y));
+
+    template <class C, class T> static T From(const T& pt, const wxSize& dpi) {
+        return T(From<C>(pt.x, dpi.x), From<C>(pt.y, dpi.y));
     }
-    template <class T> static T ToPixels(const T& pt, const wxSize& dpi) {
-        return T(ToPixels(pt.x, dpi.x), ToPixels(pt.y, dpi.y));
+    template <class C, class T> static T To(const T& pt, const wxSize& dpi) {
+        return T(To<C>(pt.x, dpi.x), To<C>(pt.y, dpi.y));
+    }
+
+    template <class C>
+    static wxRect From(const wxRect& rect, const wxSize& dpi) {
+        return wxRect(From<C>(rect.GetPosition(), dpi),
+                      From<C>(rect.GetSize(), dpi));
+    }
+    template <class C>
+    static wxRect To(const wxRect& rect, const wxSize& dpi) {
+        return wxRect(To<C>(rect.GetPosition(), dpi),
+                      To<C>(rect.GetSize(), dpi));
     }
 
 private:
-    template <class T> static bool isint(T) {
-        return std::numeric_limits<T>::is_integer;
-    }
-};
+    template <class T, bool isint = std::numeric_limits<T>::is_integer>
+    struct Trans0
+    {
+        static T From(T i, int I1, int I2) { return i * I1 / I2; }
+        static T To(T i, int I1, int I2)   { return i * I2 / I1; }
+    };
 
-template <> class Coords<0>
-{
-public:
-    enum { Inch = 0 };
+    template <class T>
+    struct Trans0<T, true>
+    {
+        static T From(T i, int I1, int I2) {
+            return (i * I1 - (i < 0 ? I2 - 1 : 0)) / I2;
+        }
+        static T To(T i, int I1, int I2) {
+            return (i * I2 + (i > 0 ? I1 - 1 : 0)) / I1;
+        }
+    };
 
-    template <class T, class U> static T FromPixels(const T& i, const U&) {
-        return i;
-    }
-    template <class T, class U> static T ToPixels(const T& i, const U&) {
-        return i;
-    }
+    template <int I1, int I2, class T> struct Trans
+    {
+        static T From(T i, int)     { return Trans0<T>::From(i, I1, I2); }
+        static T To(T i, int)       { return Trans0<T>::To(i, I1, I2); }
+    };
+
+    template <int I2, class T> struct Trans<0, I2, T>
+    {
+        static T From(T i, int I1)  { return Trans0<T>::From(i, I1, I2); }
+        static T To(T i, int I1)    { return Trans0<T>::To(i, I1, I2); }
+    };
+
+    template <int I1, class T> struct Trans<I1, 0, T>
+    {
+        static T From(T i, int I2)  { return Trans0<T>::From(i, I1, I2); }
+        static T To(T i, int I2)    { return Trans0<T>::To(i, I1, I2); }
+    };
+
+    template <class T> struct Trans<0, 0, T>
+    {
+        static T From(T i, int)     { return i; }
+        static T To(T i, int)       { return i; }
+    };
 };
 
 typedef Coords<0> Pixels;

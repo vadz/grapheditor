83f54321a28bf5682395c14643929af33118ebca
Mike Wetherell <mike.wetherell@tt-solutions.com>
2009-04-26 16:16:48 +0000

    Complete printing API.

--- include/graphprint.h
+++ include/graphprint.h
@@ -17,35 +17,16 @@
 
 namespace tt_solutions {
 
-struct Margins
-{
-    Margins(int left, int right, int top, int bottom)
-     : left(left), right(right), top(top), bottom(bottom)
-    { }
-
-    Margins(const wxPageSetupDialogData& pageSetup)
-     : left(pageSetup.GetMarginTopLeft().x),
-       right(pageSetup.GetMarginBottomRight().x),
-       top(pageSetup.GetMarginTopLeft().y),
-       bottom(pageSetup.GetMarginBottomRight().y)
-    { }
-
-    int left;
-    int right;
-    int top;
-    int bottom;
-};
-
 struct MaxPages
 {
     enum { Unlimited = 0 };
 
     MaxPages(int pages = Unlimited)
-     : rows(Unlimited), cols(Unlimited), pages(pages)
+      : rows(Unlimited), cols(Unlimited), pages(pages)
     { }
 
     MaxPages(int rows, int cols, int pages = Unlimited)
-     : rows(rows), cols(cols), pages(pages)
+      : rows(rows), cols(cols), pages(pages)
     { }
 
     int rows;
@@ -53,14 +34,121 @@ struct MaxPages
     int pages;
 };
 
+class PrintLabel
+{
+public:
+    PrintLabel(const wxString& text, int flags, int height, wxFont font)
+      : m_text(text), m_flags(flags), m_height(height), m_font(font)
+    { }
+
+    void SetText(const wxString& text) { m_text = text; }
+    wxString GetText() const { return m_text; }
+
+    void SetFont(const wxFont& font) { m_font = font; }
+    wxFont GetFont() const { return m_font; }
+
+    void SetFlags(int flags) { m_flags = flags; }
+    int GetFlags() const { return m_flags; }
+
+    int GetAlignment() const { return m_flags & wxALIGN_MASK; }
+    int GetPosition() const { return m_flags & wxALL; }
+    int GetHeight() const { return m_height; }
+
+    typedef std::list<PrintLabel> list;
+    operator list() const { return list(1, *this); }
+
+private:
+    wxString m_text;
+    int m_flags;
+    int m_height;
+    wxFont m_font;
+};
+
+typedef PrintLabel::list PrintLabels;
+
+inline PrintLabels& operator+=(PrintLabels& l1, const PrintLabels& l2)
+{
+    l1.insert(l1.end(), l2.begin(), l2.end());
+    return l1;
+}
+
+inline PrintLabels operator+(const PrintLabels& l1, const PrintLabels& l2)
+{
+    PrintLabels l(l1);
+    return l += l2;
+}
+
+inline PrintLabels Footer(
+    const wxString& text = _T("Page %PAGE% of %PAGES%"),
+    int align = wxALIGN_CENTRE,
+    int height = 12,
+    const wxFont& font = wxFont(12, wxSWISS, wxNORMAL, wxNORMAL))
+{
+    return PrintLabel(text, align | wxALIGN_BOTTOM | wxBOTTOM, height, font);
+}
+
+inline PrintLabels Header(
+    const wxString& text = _T("Page %PAGE% of %PAGES%"),
+    int align = wxALIGN_CENTRE,
+    int height = 12,
+    const wxFont& font = wxFont(12, wxSWISS, wxNORMAL, wxNORMAL))
+{
+    return PrintLabel(text, align | wxALIGN_TOP | wxTOP, height, font);
+}
+
+class GraphPages
+{
+public:
+    GraphPages(Graph *graph,
+               const wxPageSetupDialogData& setup,
+               double scale = 100,
+               MaxPages shrinktofit = MaxPages::Unlimited,
+               PrintLabels labels = Footer());
+
+    virtual ~GraphPages() { }
+
+    virtual void PreparePrinting();
+    virtual bool PrintPage(int printoutPage, int graphPage);
+
+    int GetPages() const { return m_pages.x * m_pages.y; }
+    int GetRows() const { return m_pages.y; }
+    int GetCols() const { return m_pages.x; }
+
+    wxRect GetPrintRect() const { return m_print; }
+
+    wxPrintout *GetPrintout() const { return m_printout; }
+    void SetPrintout(wxPrintout *printout) { m_printout = printout; }
+
+protected:
+    virtual void DrawLabel(wxDC *dc, const PrintLabel& label,
+                           const wxRect& rc, int page, int row, int col);
+
+private:
+    wxPrintout *m_printout;
+    Graph *m_graph;
+    double m_scale;
+    MaxPages m_max;
+    wxPageSetupDialogData m_setup;
+    wxSize m_pages;
+    wxPoint m_firstPage;
+    wxRect m_print;
+    wxRect m_header;
+    wxRect m_footer;
+    PrintLabels m_labels;
+};
+
 class GraphPrintout : public wxPrintout
 {
 public:
     GraphPrintout(Graph *graph,
-                  const Margins& margins,
+                  const wxPageSetupDialogData& setup,
                   double scale = 100,
                   MaxPages shrinktofit = MaxPages::Unlimited,
+                  PrintLabels labels = Footer(),
                   const wxString& title = _T("Graph"));
+    GraphPrintout(GraphPages *graphpages,
+                  const wxString& title = _T("Graph"));
+    ~GraphPrintout();
 
     void OnPreparePrinting();
     bool HasPage(int page);
@@ -68,13 +156,7 @@ public:
     void GetPageInfo(int *minPage, int *maxPage, int *pageFrom, int *pageTo);
 
 private:
-    Graph *m_graph;
-    double m_scale;
-    MaxPages m_max;
-    Margins m_margins;
-    wxSize m_pages;
-    wxPoint m_firstPage;
-    wxRect m_print;
+    GraphPages *m_graphpages;
 };
 
 } // namespace tt_solutions
--- src/graphprint.cpp
+++ src/graphprint.cpp
@@ -8,40 +8,109 @@
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
+#include <wx/dcprint.h>
+
 #include "graphprint.h"
 
 #undef min
+#undef max
+
 using std::min;
+using std::max;
 
 namespace tt_solutions {
 
+// ----------------------------------------------------------------------------
+// GraphPrintout
+// ----------------------------------------------------------------------------
+
 GraphPrintout::GraphPrintout(Graph *graph,
-                             const Margins& margins,
+                             const wxPageSetupDialogData& setup,
                              double scale,
                              MaxPages shrinktofit,
+                             PrintLabels labels,
                              const wxString& title)
   : wxPrintout(title),
-    m_graph(graph),
-    m_scale(scale / 100),
-    m_max(shrinktofit),
-    m_margins(margins)
+    m_graphpages(new GraphPages(graph, setup, scale, shrinktofit, labels))
 {
-    graph->UnselectAll();
+    m_graphpages->SetPrintout(this);
+}
+
+GraphPrintout::GraphPrintout(GraphPages *graphpages,
+                             const wxString& title)
+  : wxPrintout(title),
+    m_graphpages(graphpages)
+{
+    m_graphpages->SetPrintout(this);
+}
+
+GraphPrintout::~GraphPrintout()
+{
+    delete m_graphpages;
 }
 
 void GraphPrintout::OnPreparePrinting()
 {
+    m_graphpages->PreparePrinting();
+}
+
+bool GraphPrintout::HasPage(int page)
+{
+    return page >= 1 && page <= m_graphpages->GetPages();
+}
+
+bool GraphPrintout::OnPrintPage(int page)
+{
+    return HasPage(page) && m_graphpages->PrintPage(page, page);
+}
+
+void GraphPrintout::GetPageInfo(int *minPage,
+                                int *maxPage,
+                                int *pageFrom,
+                                int *pageTo)
+{
+    *minPage = 1;
+    *maxPage = m_graphpages->GetPages();
+    *pageFrom = 1;
+    *pageTo = *maxPage;
+}
+
+// ----------------------------------------------------------------------------
+// GraphPages
+// ----------------------------------------------------------------------------
+
+GraphPages::GraphPages(Graph *graph,
+                       const wxPageSetupDialogData& setup,
+                       double scale,
+                       MaxPages shrinktofit,
+                       PrintLabels labels)
+  : m_printout(NULL),
+    m_graph(graph),
+    m_scale(scale / 100),
+    m_max(shrinktofit),
+    m_setup(setup),
+    m_labels(labels)
+{
+}
+
+void GraphPages::PreparePrinting()
+{
+    wxASSERT(m_printout);
+    wxASSERT(m_graph);
+
+    m_graph->UnselectAll();
+
     int xdpi, ydpi;
-    GetPPIPrinter(&xdpi, &ydpi);
+    m_printout->GetPPIPrinter(&xdpi, &ydpi);
 
     // convert the margins into printer pixels
-    int left   = MM::To<Pixels>(m_margins.left, xdpi);
-    int right  = MM::To<Pixels>(m_margins.right, xdpi);
-    int top    = MM::To<Pixels>(m_margins.top, ydpi);
-    int bottom = MM::To<Pixels>(m_margins.bottom, ydpi);
+    int left   = MM::To<Pixels>(m_setup.GetMarginTopLeft().x, xdpi);
+    int right  = MM::To<Pixels>(m_setup.GetMarginBottomRight().x, xdpi);
+    int top    = MM::To<Pixels>(m_setup.GetMarginTopLeft().y, ydpi);
+    int bottom = MM::To<Pixels>(m_setup.GetMarginBottomRight().y, ydpi);
 
     // the printing rect is then the paper size less the margins
-    m_print = GetPaperRectPixels();
+    m_print = m_printout->GetPaperRectPixels();
     m_print.x += left;
     m_print.y += top;
     m_print.width -= left + right;
@@ -49,8 +118,30 @@ void GraphPrintout::OnPreparePrinting()
 
     // intersect this with the printer's printable area
     int wprintable, hprintable;
-    GetPageSizePixels(&wprintable, &hprintable);
+    m_printout->GetPageSizePixels(&wprintable, &hprintable);
     m_print.Intersect(wxRect(0, 0, wprintable, hprintable));
+
+    // calculate the height of the page headers and footers
+    int header = 0, footer = 0;
+    PrintLabels::const_iterator it;
+
+    for (it = m_labels.begin(); it != m_labels.end(); ++it) {
+        int height = MM::To<Pixels>(it->GetHeight(), ydpi);
+        int position = it->GetPosition();
+
+        if (position == wxTOP)
+            header = max(header, height);
+        else if (position == wxBOTTOM)
+            footer = max(footer, height);
+    }
+
+    m_header = m_print;
+    m_print.y += header;
+    m_print.height -= header + footer;
+    m_header.height = header;
+    m_footer = m_print;
+    m_footer.y += m_print.height;
+    m_footer.height = footer;
 
     // size of graph in inches
     wxRect rcGraph = m_graph->GetBounds();
@@ -94,29 +185,57 @@ void GraphPrintout::OnPreparePrinting()
     m_firstPage.y = rcGraph.y + int((rcGraph.height - h * m_pages.y) / 2);
 }
 
-bool GraphPrintout::HasPage(int page)
+void GraphPages::DrawLabel(wxDC *dc, const PrintLabel& label,
+                           const wxRect& rc, int page, int row, int col)
 {
-    return page > 0 && page <= m_pages.x * m_pages.y;
+    wxString text = label.GetText();
+    text.Replace(_T("%ROW%"),   wxString() << row);
+    text.Replace(_T("%ROWS%"),  wxString() << GetRows());
+    text.Replace(_T("%COL%"),   wxString() << col);
+    text.Replace(_T("%COLS%"),  wxString() << GetCols());
+    text.Replace(_T("%PAGE%"),  wxString() << page);
+    text.Replace(_T("%PAGES%"), wxString() << GetPages());
+
+    dc->SetFont(label.GetFont());
+    dc->DrawLabel(text, rc, label.GetAlignment());
 }
 
-bool GraphPrintout::OnPrintPage(int page)
+bool GraphPages::PrintPage(int printoutPage, int graphPage)
 {
-    wxDC *dc = GetDC();
-    if (!dc || !HasPage(page))
+    wxDC *dc = m_printout->GetDC();
+    if (!dc || graphPage < 1)
         return false;
 
-    page--;
-    int xpage = page % m_pages.x;
-    int ypage = page / m_pages.x;
+    --graphPage;
+    int xpage = graphPage % m_pages.x;
+    int ypage = graphPage / m_pages.x;
 
     int wpage, hpage;
-    GetPageSizePixels(&wpage, &hpage);
+    m_printout->GetPageSizePixels(&wpage, &hpage);
 
     int wdc, hdc;
     dc->GetSize(&wdc, &hdc);
 
     int xdpi, ydpi;
-    GetPPIPrinter(&xdpi, &ydpi);
+    m_printout->GetPPIPrinter(&xdpi, &ydpi);
+
+    dc->SetUserScale(double(wdc) / wpage, double(hdc) / hpage);
+    dc->SetDeviceOrigin(0, 0);
+    dc->SetLogicalOrigin(0, 0);
+
+    PrintLabels::const_iterator it;
+
+    for (it = m_labels.begin(); it != m_labels.end(); ++it) {
+        int position = it->GetPosition();
+        wxRect rc;
+
+        if (position == wxTOP)
+            rc = m_header;
+        else if (position == wxBOTTOM)
+            rc = m_footer;
+
+        DrawLabel(dc, *it, rc, printoutPage, ypage + 1, xpage + 1);
+    }
 
     wxSize dpiGraph = m_graph->GetDPI();
 
@@ -147,17 +266,6 @@ bool GraphPrintout::OnPrintPage(int page)
     m_graph->Draw(dc, rcPage);
 
     return true;
-}
-
-void GraphPrintout::GetPageInfo(int *minPage,
-                                int *maxPage,
-                                int *pageFrom,
-                                int *pageTo)
-{
-    *minPage = 1;
-    *maxPage = m_pages.x * m_pages.y;
-    *pageFrom = 1;
-    *pageTo = *maxPage;
 }
 
 } // namespace tt_solutions

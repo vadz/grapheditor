vadz/grapheditor

commit 40e3bfe0175c6be83db89707260644b47537b55e

Avoid vc++ warning.

include/archive.h
@@ -196,7 +196,7 @@ bool Extract(const Archive::Item& arc, const wxString& name, T& value)
     if (ss)
         value = val;
 
-    return ss;
+    return ss != NULL;
 }
 
 } // namespace tt_solutions

commit 91d83df1f034066d1ea7bc7696969cc8f01d0abc

Compilation fix for stream-to-bool conversion
This was broken in 40e3bfe0175c6be83db89707260644b47537b55e when
attempting to fix MSVC warning: with MSVS 2017 this code actually gives
an error, as it should, because a stream can't be compared with NULL.

Do what the original code intended by explicitly using good().

include/archive.h
@@ -731,7 +731,7 @@ bool Extract(const Archive::Item& arc, const wxString& name, T& value)
     if (ss)
         value = val;
 
-    return ss != NULL;
+    return ss.good();
 }
 //@}
 
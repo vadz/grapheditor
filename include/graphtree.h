vadz/grapheditor

commit c65605f7d91da5ee9772289b80ed66cd68ed247e

Mark overridden virtual functions with "override"
Use gcc -Wsuggest-override option to find all the overridden functions
and mark them with "override" to ensure that they don't get silently
broken by the changes to the base classes in the future.

include/graphtree.h
@@ -54,7 +54,7 @@ public:
      *
      * This interferes with drag and drop.
      */
-    bool SendAutoScrollEvents(wxScrollWinEvent&) const { return false; }
+    bool SendAutoScrollEvents(wxScrollWinEvent&) const override { return false; }
 
     /// Event handler for begin drag event.
 
@@ -138,7 +138,7 @@ public:
     void SetIcon(const wxIcon& icon) { m_icon = icon; }
 
     /** @brief Clone. */
-    wxEvent *Clone() const { return new GraphTreeEvent(*this); }
+    wxEvent *Clone() const override { return new GraphTreeEvent(*this); }
 
 private:
     GraphCtrl *m_target;    ///< Target of the drop.

include/factory.h
@@ -280,7 +280,7 @@ public:
          *
          * Notice that a covariant return type is used here.
          */
-        T *New() const {
+        T *New() const override {
             return new T(*GetDefault());
         }
 
@@ -289,7 +289,7 @@ public:
          *
          * Notice that a covariant return type is used here.
          */
-        const T *GetDefault() const {
+        const T *GetDefault() const override {
             if (!m_default)
                 m_default = new T;
             return m_default;

include/graphctrl.h
@@ -626,7 +626,7 @@ public:
      *
      * Invalidates any iterators pointing to this element.
      */
-    virtual void SetStyle(int style);
+    void SetStyle(int style) override;
 
     //@{
     /** @brief Size of the arrow head, if present. */
@@ -689,7 +689,7 @@ public:
      * Can be overridden in a derived class to handle any additional
      * attributes.
      */
-    bool Serialise(Archive::Item& arc);
+    bool Serialise(Archive::Item& arc) override;
 
     /**
      * @brief Get the associated graphic object from the underlying graphics
@@ -724,13 +724,13 @@ public:
      * Such as SetSize(), SetText() and SetFont(), to give the element the
      * chance to adjust its layout. The default implementation does nothing.
      */
-    virtual void Layout() { }
+    void Layout() override { }
 
     /** @brief Overridable returning the pen that will be used. */
-    wxPen GetPen() const { return wxPen(GetColour(), m_linewidth); }
+    wxPen GetPen() const override { return wxPen(GetColour(), m_linewidth); }
 
 protected:
-    virtual void UpdateShape() { }
+    void UpdateShape() override { }
 
     /**
      * Move this edge to the front of the list of shapes.
@@ -873,7 +873,7 @@ public:
      *
      * Invalidates any iterators pointing to this element.
      */
-    virtual void SetStyle(int style);
+    void SetStyle(int style) override;
 
     //@{
     /**
@@ -942,7 +942,7 @@ public:
      * Can be overridden in a derived class to handle any additional
      * attributes.
      */
-    bool Serialise(Archive::Item& arc);
+    bool Serialise(Archive::Item& arc) override;
 
     /**
      * @brief Move the node, centering it on the given point.
@@ -977,7 +977,7 @@ public:
      *
      * Invalidates any iterators pointing to this element.
      */
-    void SetShape(wxShape *shape);
+    void SetShape(wxShape *shape) override;
 
     /**
      * @brief This can be overridden to give the node a custom shape.
@@ -1000,8 +1000,8 @@ public:
                                       const wxPoint& outside) const;
 
 protected:
-    virtual void DoSelect(bool select);
-    virtual void UpdateShape();
+    void DoSelect(bool select) override;
+    void UpdateShape() override;
 
     /**
      * Ensure that we use the correct text colour.
@@ -1031,7 +1031,7 @@ protected:
      * to adjust its layout. It calls OnLayout() to allow derived classes
      * to take care of any custom features they add.
      */
-    virtual void Layout();
+    void Layout() override;
 
 private:
     /// Get the list of all underlying lines connecting to this node.
@@ -1222,7 +1222,7 @@ public:
      *
      * Affected by @c SetMargin().
      */
-    virtual void Fit();
+    void Fit() override;
 
     /**
      * @brief The kind of border the scrollbars leave around the graph.
@@ -1449,7 +1449,7 @@ public:
 
 protected:
     /** Returns the DPI used by the control. */
-    virtual wxSize GetDPI() const;
+    wxSize GetDPI() const override;
 
 private:
     /**
@@ -2224,7 +2224,7 @@ public:
     GraphEvent(const GraphEvent& event);
 
     /** @brief Clone. */
-    virtual wxEvent *Clone() const      { return new GraphEvent(*this); }
+    wxEvent *Clone() const override { return new GraphEvent(*this); }
 
     /**
      * @brief The node being added, deleted, clicked, etc.

include/graphprint.h
@@ -356,10 +356,10 @@ public:
      * corresponding methods of GraphPages.
      */
     //@{
-    void OnPreparePrinting();
-    bool HasPage(int page);
-    bool OnPrintPage(int page);
-    void GetPageInfo(int *minPage, int *maxPage, int *pageFrom, int *pageTo);
+    void OnPreparePrinting() override;
+    bool HasPage(int page) override;
+    bool OnPrintPage(int page) override;
+    void GetPageInfo(int *minPage, int *maxPage, int *pageFrom, int *pageTo) override;
     //@}
 
 private:

include/projectdesigner.h
@@ -55,8 +55,8 @@ public:
     /** @brief Destructor. */
     ~ProjectNode();
 
-    void SetText(const wxString& text);
-    void SetFont(const wxFont& font);
+    void SetText(const wxString& text) override;
+    void SetFont(const wxFont& font) override;
 
     //@{
     /** @brief The node's id. */
@@ -87,7 +87,7 @@ public:
      * Can be overridden in a derived class to handle any additional
      * attributes.
      */
-    bool Serialise(tt_solutions::Archive::Item& arc);
+    bool Serialise(tt_solutions::Archive::Item& arc) override;
 
     /**
      * @brief Indicates what part of the node is at the given point, for
@@ -145,11 +145,11 @@ public:
     void SetMaxAutoSize(const wxSize& size) { m_maxAutoSize = size; }
     /** @endcond */
 
-    void OnDraw(wxDC& dc);
-    void OnLayout(wxDC &dc);
+    void OnDraw(wxDC& dc) override;
+    void OnLayout(wxDC &dc) override;
 
     wxPoint GetPerimeterPoint(const wxPoint& inside,
-                              const wxPoint& outside) const;
+                              const wxPoint& outside) const override;
 
 protected:
     /**
@@ -224,7 +224,7 @@ public:
     void DrawCanvasBackground(wxDC& dc);
 
     /** @brief Sets the background colour of the control. */
-    bool SetBackgroundColour(const wxColour& colour);
+    bool SetBackgroundColour(const wxColour& colour) override;
     /** @brief Sets the background colour gradient of the control. */
     void SetBackgroundGradient(const wxColour& from, const wxColour& to);
 

include/tipwin.h
@@ -57,14 +57,14 @@ public:
      * implement this by explicitly forwarding any such events that we get to
      * our parent window.
      */
-    bool ProcessEvent(wxEvent& event);
+    bool ProcessEvent(wxEvent& event) override;
 
     /**
      * @brief Override the base class method to refuse focus.
      *
      * A tooltip window doesn't accept input and hence doesn't need the focus.
      */
-    bool AcceptsFocus() const { return false; }
+    bool AcceptsFocus() const override { return false; }
 
     /**
      * @brief Paint event handler.
@@ -84,7 +84,7 @@ protected:
      */
     void DoSetSize(int x, int y,
                    int width, int height,
-                   int sizeFlags = wxSIZE_AUTO);
+                   int sizeFlags = wxSIZE_AUTO) override;
 
 private:
     /// A possibly multiline text to show.

samples/graphtest.cpp
@@ -378,8 +378,8 @@ public:
     // this one is called on application startup and is a good place for the app
     // initialization (doing it here and not in the ctor allows to have an error
     // return: if OnInit() returns false, the application terminates)
-    virtual bool OnInit();
-    virtual int OnExit();
+    bool OnInit() override;
+    int OnExit() override;
 };
 
 // Define a new frame type: this is going to be our main frame
@@ -832,6 +832,11 @@ MyFrame::MyFrame(const wxString& title)
     m_printPosX = .5;
     // and towards the top, split free space 30% / 70%
     m_printPosY = .3;
+
+#ifndef FIXME_VZ
+    m_graph->Add(new SampleNode(), wxPoint(100, 100));
+    m_graph->Add(new SampleNode(), wxPoint(1000, 1000));
+#endif // FIXME_VZ
 }
 
 MyFrame::~MyFrame()

src/graphctrl.cpp
@@ -390,14 +390,14 @@ public:
      *
      * Deselects all items and generates a Evt_Graph_Click event.
      */
-    void OnLeftClick(double x, double y, int keys);
+    void OnLeftClick(double x, double y, int keys) override;
 
     /**
      * Called when right mouse button is clicked.
      *
      * Generates a Evt_Graph_Menu event.
      */
-    void OnRightClick(double x, double y, int keys);
+    void OnRightClick(double x, double y, int keys) override;
 
     /**
      * Called when mouse starts to move with left button down.
@@ -405,21 +405,21 @@ public:
      * Sets up the variables used by OnDragLeft(): starts panning if Shift key
      * is pressed or rubber-banding otherwise.
      */
-    void OnBeginDragLeft(double x, double y, int keys);
+    void OnBeginDragLeft(double x, double y, int keys) override;
 
     /**
      * Called when mouse is moved with left button down.
      *
      * Implements feedback for panning and rubber-banding.
      */
-    void OnDragLeft(bool draw, double x, double y, int keys);
+    void OnDragLeft(bool draw, double x, double y, int keys) override;
 
     /**
      * Called when the left mouse button is released after dragging.
      *
      * Finalizes a panning or rubber-banding operation.
      */
-    void OnEndDragLeft(double x, double y, int keys);
+    void OnEndDragLeft(double x, double y, int keys) override;
     //@}
 
     /**
@@ -442,7 +442,7 @@ public:
     /**
      * Override event processing to send mouse events to the parent.
      */
-    bool ProcessEvent(wxEvent& event);
+    bool ProcessEvent(wxEvent& event) override;
 
     /**
      * Release mouse if we currently have the capture.
@@ -534,7 +534,7 @@ public:
      * The scrollbars will be set from CheckBounds() called from
      * GraphCtrl::OnIdle() instead.
      */
-    void AdjustScrollbars() { }
+    void AdjustScrollbars() override { }
     //@}
 
     /**
@@ -1322,8 +1322,8 @@ public:
 
     /// Overridden base class virtual method.
     //@{
-    void OnErase(wxDC& dc);
-    void OnMoveLink(wxDC& dc, bool moveControlPoints);
+    void OnErase(wxDC& dc) override;
+    void OnMoveLink(wxDC& dc, bool moveControlPoints) override;
     //@}
 
 protected:
@@ -1392,7 +1392,7 @@ public:
     ControlPointHandler(wxShapeEvtHandler *prev);
 
     /// Overridden base class virtual method.
-    void OnErase(wxDC& dc);
+    void OnErase(wxDC& dc) override;
 };
 
 ControlPointHandler::ControlPointHandler(wxShapeEvtHandler *prev)
@@ -1421,12 +1421,12 @@ public:
 
     /// Overridden base class virtual method.
     //@{
-    void OnLeftClick(double x, double y, int keys, int attachment);
-    void OnLeftDoubleClick(double x, double y, int keys, int attachment);
-    void OnRightClick(double x, double y, int keys, int attachment);
+    void OnLeftClick(double x, double y, int keys, int attachment) override;
+    void OnLeftDoubleClick(double x, double y, int keys, int attachment) override;
+    void OnRightClick(double x, double y, int keys, int attachment) override;
 
-    void OnDraw(wxDC& dc)       { GetElement(GetShape())->OnDraw(dc); }
-    void OnDrawContents(wxDC&)  { }
+    void OnDraw(wxDC& dc) override { GetElement(GetShape())->OnDraw(dc); }
+    void OnDrawContents(wxDC&) override { }
     //@}
 
 protected:
@@ -1557,29 +1557,36 @@ public:
 
     /// Overridden base class virtual method.
     //@{
-    void OnLeftClick(double x, double y, int keys, int attachment);
-    void OnLeftDoubleClick(double x, double y, int keys, int attachment);
-    void OnRightClick(double x, double y, int keys, int attachment);
+    void OnLeftClick(double x, double y, int keys, int attachment) override;
+    void OnLeftDoubleClick(double x, double y, int keys, int attachment) override;
+    void OnRightClick(double x, double y, int keys, int attachment) override;
 
-    void OnBeginDragLeft(double x, double y, int keys, int attachment);
-    void OnDragLeft(bool draw, double x, double y, int keys, int attachment);
-    void OnEndDragLeft(double x, double y, int keys, int attachment);
+    void OnBeginDragLeft(double x, double y, int keys, int attachment) override;
+    void OnDragLeft(bool draw, double x, double y, int keys, int attachment) override;
+    void OnEndDragLeft(double x, double y, int keys, int attachment) override;
 
-    void OnBeginDragRight(double x, double y, int keys, int attachment);
-    void OnDragRight(bool draw, double x, double y, int keys, int attachment);
-    void OnEndDragRight(double x, double y, int keys, int attachment);
+    void OnBeginDragRight(double x, double y, int keys, int attachment) override;
+    void OnDragRight(bool draw, double x, double y, int keys, int attachment) override;
+    void OnEndDragRight(double x, double y, int keys, int attachment) override;
 
     void OnBeginDrag(int mode, double x, double y);
     void OnDrag(int mode, bool draw, double x, double y);
     void OnEndDrag(int mode, double x, double y);
 
-    void OnEraseContents(wxDC& dc)  { GraphElementHandler::OnErase(dc); }
-    void OnErase(wxDC& dc)          { wxShapeEvtHandler::OnErase(dc); }
+    void OnEraseContents(wxDC& dc) override
+    {
+        GraphElementHandler::OnErase(dc);
+    }
 
-    void OnDraw(wxDC& dc);
+    void OnErase(wxDC& dc) override
+    {
+        wxShapeEvtHandler::OnErase(dc);
+    }
 
-    void OnSizingDragLeft(wxControlPoint* pt, bool draw, double x, double y, int keys, int attachment);
-    void OnSizingEndDragLeft(wxControlPoint* pt, double x, double y, int keys, int attachment);
+    void OnDraw(wxDC& dc) override;
+
+    void OnSizingDragLeft(wxControlPoint* pt, bool draw, double x, double y, int keys, int attachment) override;
+    void OnSizingEndDragLeft(wxControlPoint* pt, double x, double y, int keys, int attachment) override;
     //@}
 
     /// Return the associated GraphNode.
@@ -1863,7 +1870,7 @@ public:
 protected:
     /// Overridden base class virtual method.
     //@{
-    wxRect GetEraseRect() const;
+    wxRect GetEraseRect() const override;
     inline wxLineShape *GetShape() const;
     //@}
 };
@@ -1915,7 +1922,7 @@ public:
     /// Overridden base class virtual method.
     bool GetPerimeterPoint(double x1, double y1,
                            double x2, double y2,
-                           double *x3, double *y3);
+                           double *x3, double *y3) override;
 };
 
 bool GraphNodeShape::GetPerimeterPoint(double x1, double y1,
@@ -1965,14 +1972,14 @@ public:
      *
      * Calls SetEventHandler() and the base class method.
      */
-    void AddShape(wxShape *shape, wxShape *addAfter = NULL);
+    void AddShape(wxShape *shape, wxShape *addAfter = NULL) override;
 
     /**
      * Override to set up a correct handler for @a shape.
      *
      * Calls SetEventHandler() and the base class method.
      */
-    void InsertShape(wxShape *shape);
+    void InsertShape(wxShape *shape) override;
 
     /**
      * Associate an appropriate custom event handler with the shape.
@@ -1987,7 +1994,7 @@ public:
      * Override Redraw since the default method displays a busy cursor which
      * flashes on and off during panning.
      */
-    void Redraw(wxDC& dc);
+    void Redraw(wxDC& dc) override;
 };
 
 // The custom behaviour of the wxShapes is achieved using wxShapeEvtHandler
@@ -2186,7 +2193,7 @@ public:
     /**
      * Advance the iterator skipping over unacceptable elements.
      */
-    void inc()
+    void inc() override
     {
         do {
             ++m_it;
@@ -2197,7 +2204,7 @@ public:
     /**
      * Advance the iterator backwards skipping over unacceptable elements.
      */
-    void dec()
+    void dec() override
     {
         do {
             --m_it;
@@ -2210,20 +2217,20 @@ public:
      *
      * The comparison doesn't take filter settings into account.
      */
-    bool eq(const GraphIteratorImpl& other) const
+    bool eq(const GraphIteratorImpl& other) const override
     {
         return typeid(other) == typeid(ListIterImpl) &&
                m_it == static_cast<const ListIterImpl&>(other).m_it;
     }
 
     /// Dereference the iterator.
-    GraphElement *get() const
+    GraphElement *get() const override
     {
         return GetElement(wxStaticCast(*m_it, wxShape));
     }
 
     /// Create a copy of this iterator polymorphically.
-    ListIterImpl *clone() const
+    ListIterImpl *clone() const override
     {
         return new ListIterImpl(*this);
     }
@@ -2279,7 +2286,7 @@ public:
     /**
      * Advance the iterator skipping over unacceptable elements.
      */
-    void inc()
+    void inc() override
     {
         do {
             m_pos++;
@@ -2290,7 +2297,7 @@ public:
     /**
      * Advance the iterator backwards skipping over unacceptable elements.
      */
-    void dec()
+    void dec() override
     {
         do {
             m_pos--;
@@ -2304,7 +2311,7 @@ public:
      * The comparison doesn't take filter settings (i.e. type of the elements
      * to accept) into account.
      */
-    bool eq(const GraphIteratorImpl& other) const
+    bool eq(const GraphIteratorImpl& other) const override
     {
         if (typeid(other) != typeid(PairIterImpl))
             return false;
@@ -2313,13 +2320,13 @@ public:
     }
 
     /// Dereference the iterator.
-    GraphElement *get() const
+    GraphElement *get() const override
     {
         return GetElement(m_pos == 1 ? m_line->GetFrom() : m_line->GetTo());
     }
 
     /// Create a copy of this iterator polymorphically.
-    PairIterImpl *clone() const
+    PairIterImpl *clone() const override
     {
         return new PairIterImpl(*this);
     }

commit 250f0b04644772dd374f5cdd970511aac2d71e4a

Fix wrong use of "override" in GraphTreeCtrl under MSW
Fix build error under MSW introduced in c65605f (Mark overridden virtual
functions with "override", 2024-07-21) due to the fact that function
being overridden only exists in the generic version of wxTreeCtrl.

include/graphtree.h
@@ -49,12 +49,14 @@ public:
         Init();
     }
 
+#ifdef wxHAS_GENERIC_TREECTRL
     /**
      * @brief Override base class function to avoid auto scrolling.
      *
      * This interferes with drag and drop.
      */
     bool SendAutoScrollEvents(wxScrollWinEvent&) const override { return false; }
+#endif // wxHAS_GENERIC_TREECTRL
 
     /// Event handler for begin drag event.
 